5b715a2861ee03747e2ddf6969df2c6fcfdb38b1
zaurbbb <zaurberdibekov@mail.ru>
2022-11-25 05:46:52 +0600

    create three Navbar components

---
+++ src/components/elements/Header/CategoriesNavbar/CategoriesNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoriesNavbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CategoriesNavbar;
---
+++ src/components/elements/Header/SecondaryNavbar/SecondaryNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SecondaryNavbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SecondaryNavbar;
