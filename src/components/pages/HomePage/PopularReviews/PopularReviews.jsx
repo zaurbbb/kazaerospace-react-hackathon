d5ee75d59d585610bf29ddb00d966ac6abbc75ba
zaurbbb <zaurberdibekov@mail.ru>
2022-11-25 15:23:08 +0600

    update dependencies (install swiper)

---
+++ src/components/pages/HomePage/PopularReviews/PopularReviews.jsx
@@ -0,0 +1,15 @@
+//импорт библиотек
+import React from 'react';
+
+//импорт стилей
+import css from './PopularReviews.module.sass'
+
+const PopularReviews = () => {
+    return (
+        <section>
+
+        </section>
+    );
+};
+
+export default PopularReviews;
